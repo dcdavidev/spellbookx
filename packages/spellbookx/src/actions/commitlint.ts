fb5ce1fee08ad05aa28fc3e793713e039457a8aa
Davide Di Criscito <davidedc@outlook.com>
2025-11-13 11:08:07 +0100

    fix: update commitlint configuration to use 'spellbookx'

--- packages/spellbookx/src/actions/commitlint.ts
+++ packages/spellbookx/src/actions/commitlint.ts
@@ -98,7 +98,7 @@ export function actionCommitlint() {
   const cwd = process.cwd();
   const commitlintConfigPath = path.join(cwd, 'commitlint.config.mjs');
   const commitlintConfig = `export default {
-  extends: ['commitlint-config-spellbookx'],
+  extends: ['spellbookx'],
 };
 `;
 
